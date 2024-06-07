d75c1a516709f01a1dc4fa7399ba576a91abdd4f
Krishna Singh <krisingh18@publicisgroupe.net>
2024-06-07 14:17:33 +0530

    commit

---
+++ mongodbcrud/src/main/java/com/mongodb/repository/StudentRepository.java
@@ -0,0 +1,27 @@
+package com.mongodb.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.entity.Student;
+
+
+@Repository
+public interface StudentRepository extends MongoRepository<Student, String> {
+
+	List<Student> findByName(String name);
+	
+	Student findByEmailAndName (String email, String name);
+	
+	Student findByNameOrEmail (String name, String email);
+	
+	List<Student> findByDepartmentDepartmentName(String deptname);
+	
+	List<Student> findBySubjectsSubjectName (String subName);
+	
+	List<Student> findByEmailIsLike (String email);
+	
+	List<Student> findByNameStartsWith (String name);
+}
